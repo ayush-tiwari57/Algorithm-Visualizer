ff125a4faf9e0f1699cbab855b50f8f2ae9b2fc3
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-09-14 17:52:30 +0530

    updated eventListener

--- public/js/main.js
+++ public/js/main.js
@@ -8,11 +8,22 @@ var refreshbtn = document.querySelector('.refresh');
 var startbtn = document.querySelector('.start');
 var container = document.querySelector('.container');
 var algorithm = 'dijkstra';
+export var mouseIsDown = false;
 
 //Initializing eventListeners
 refreshbtn.addEventListener('click', refresh);
 startbtn.addEventListener('click', start);
-container.addEventListener('click', setWallAttribute);
+container.addEventListener('click', function(){
+	if(mouseIsDown){
+		mouseIsDown =false;
+	}
+	else{
+		mouseIsDown = true;
+	}
+	console.log(mouseIsDown);
+})
+// container.addEventListener('mouseup', function(){mouseIsDown = false})
+container.addEventListener('mouseover', setWallAttribute);
 
 // refresh function
 function refresh() {
--- public/js/wall.js
+++ public/js/wall.js
@@ -1,19 +1,21 @@
 //Wall
-
+import { mouseIsDown } from "./main.js";
 // function for setting wall attribute
 export function setWallAttribute(event) {
 	// console.log(event);
-	if (event.target.classList.contains('node')) {
-		let node = document.querySelector('.node');
-		const row = event.target.getAttribute('row');
-		const col = event.target.getAttribute('col');
-		event.target.classList.toggle('wall');
-		console.log('clicked');
-	}
-	if (event.target.classList.contains('wall')) {
-		event.target.setAttribute('wall', 1);
-	} else {
-		event.target.setAttribute('wall', 0);
+	if(mouseIsDown){
+		if (event.target.classList.contains('node')) {
+			let node = document.querySelector('.node');
+			const row = event.target.getAttribute('row');
+			const col = event.target.getAttribute('col');
+			event.target.classList.toggle('wall');
+			console.log('clicked');
+		}
+		if (event.target.classList.contains('wall')) {
+			event.target.setAttribute('wall', 1);
+		} else {
+			event.target.setAttribute('wall', 0);
+		}
 	}
 }
 
