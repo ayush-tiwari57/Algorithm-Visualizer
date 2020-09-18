4dd4a17c212c02e8cd535842dff7abb893a91b1f
anubhavitis <ss.lfsgd@gmail.com>
2020-09-18 15:28:48 +0530

    Sorting array made

---
+++ public/sorting/js/main_sort.js
@@ -0,0 +1,26 @@
+var ArrayBox = document.getElementById("ArrayBox");
+var arr = []
+var divs = [];
+ArrayBox.style = "flex-direction:row";
+
+function GenerateArr(n) {
+    ArrayBox.innerHTML = "";
+    ArrayBox.style="width: 50%; margin:auto; ";
+    for (var i = 0; i < n; i++) {
+        arr[i] = (Math.floor(Math.random()*500) + 500) % 100;
+        divs[i] = document.createElement("div");
+        ArrayBox.appendChild(divs[i]);
+
+        var margin_size = 0.1;
+        var width = 100 / n - 2 * margin_size;
+
+        divs[i].style =
+            " margin: 0% " + margin_size + "%;" +
+            " background: grey; width:" + width + "%;" +
+            "box-shadow: 3px 3px 5px grey, 3px 3px 5px white; "+
+            " height:" + arr[i] + "%;";
+        console.log(arr[i])
+    }
+}
+
+window.onload = GenerateArr(100);
