312626874d057bcc4f57e5bb69ff1fa20c4a1ef7
eslam <esla.ahme@hotmail.com>
2020-10-05 00:00:04 +0200

    red color represent the heap building process green color represent the sorted items

--- public/sorting/js/heap_sort.js
+++ public/sorting/js/heap_sort.js
@@ -7,7 +7,6 @@ import {
 var c_delay = 1;
 var delay_time = 40 - 2 * (speed - 1);
 
-
 export function heap(divs, height) {
     c_delay = 50;
     delay_time = 10000 / (Math.floor(n / 30) * speed);
@@ -37,6 +36,7 @@ export function heap(divs, height) {
  */
 function heapify(divs, arr, size, start) {
     let max = start, left = start * 2 + 1, right = start * 2 + 2;
+    div_update(divs[2 * start + 1], arr[start], "red");
     if (left < size && arr[max] < arr[left])
         max = left;
 
@@ -45,7 +45,8 @@ function heapify(divs, arr, size, start) {
 
     if (max != start) {
         [arr[max], arr[start]] = [arr[start], arr[max]];
-
+        div_update(divs[2 * start + 1], arr[start], "red");
+        div_update(divs[2 * max + 1], arr[max], "#808080");
         heapify(divs, arr, size, max);
     }
 }
